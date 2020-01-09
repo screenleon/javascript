94aaeb61a53e9376c110d7c98cad815fc899b673
Lien Chen <screen.leon@gmail.com>
2020-01-09 08:34:26 +0800

    remove the firebase api
    remove the firebase api

--- angular/firebaseanalyticsdemo/src/environments/environment.ts
+++ angular/firebaseanalyticsdemo/src/environments/environment.ts
@@ -5,14 +5,6 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAgiyaehby3bJ3L3w2u1rqmRtFEb_cRhg0",
-    authDomain: "analytics-1cadd.firebaseapp.com",
-    databaseURL: "https://analytics-1cadd.firebaseio.com",
-    projectId: "analytics-1cadd",
-    storageBucket: "analytics-1cadd.appspot.com",
-    messagingSenderId: "300544441251",
-    appId: "1:300544441251:web:77f4c1d8c24a423e6cf816",
-    measurementId: "G-M28PKJ2791"
   }
 };
 
