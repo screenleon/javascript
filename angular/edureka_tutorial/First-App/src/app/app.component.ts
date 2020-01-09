3b9e97149a168800b4f7bd8b1622e5729cdb6cca
screenleon <screen.leon@gmail.com>
2020-01-09 15:11:08 +0800

    using directive with ngStyle, ngClass, ngFor ngStyle change the css ngClass can set the class ngIf can use the if-else method ngFor use the for loop

--- angular/edureka_tutorial/First-App/src/app/app.component.ts
+++ angular/edureka_tutorial/First-App/src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = 'First-App';
+  flag: boolean = true;
+
+  studentRoster = ['Arya', 'Lien', 'Rick'];
+  currentName = '';
+
+  toggleFlag(){
+    this.flag = !this.flag;
+  }
+
+  getColor() {
+    if(this.flag === true){
+      return 'green';
+    }else {
+      return 'purple';
+    }
+  }
+
+  add() {
+    this.studentRoster.push(this.currentName);
+    this.currentName = "";
+  }
 }
