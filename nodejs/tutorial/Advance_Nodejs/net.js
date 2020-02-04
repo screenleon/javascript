78607823633624e65774cdc676171be4c9b3687b
screenleon <screen.leon@gmail.com>
2020-02-04 11:43:48 +0800

    create a socket server to chat, use the nc to connect it

---
+++ nodejs/tutorial/Advance_Nodejs/net.js
@@ -0,0 +1,40 @@
+process.stdout.write('\u001B[2J\u001B[0;0f')
+
+const server = require('net').createServer();
+let counter = 0;
+let sockets = {};
+
+//monent library
+function timestamp() {
+    const now = new Date();
+    return `${now.getHours()}:${now.getMinutes()}`;
+}
+
+server.on('connection', socket => {
+    socket.id = counter++;
+
+    console.log('Client connected');
+    socket.write('Please type your name: ');
+
+    socket.on('data', data => {
+        if(!sockets[socket.id]){
+            socket.name = data.toString().trim();
+            socket.write(`Welcome ${socket.name}!\n`)
+            sockets[socket.id] = socket;
+            return;
+        }
+        Object.entries(sockets).forEach(([key, cs]) => {
+            if(socket.id == key)return;
+            cs.write(`${socket.name} ${timestamp()}: `);
+            cs.write(data);
+        });
+    });
+
+    socket.on('end', () => {
+        console.log('client disconnected');
+    });
+});
+
+server.listen(8000, () => {
+    console.log('Server bound');
+});
