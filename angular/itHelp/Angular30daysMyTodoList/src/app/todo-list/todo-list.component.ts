63bda5df0cd4252cff9a887427906c51eef1dd4e
screenleon <screen.leon@gmail.com>
2020-02-27 18:31:56 +0800

    新增 todo-list 模組跟組件, 現在可以輸入input, 會顯示在console.log

---
+++ angular/itHelp/Angular30daysMyTodoList/src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  /**
+   * 新增代辦事項
+   * @param {HTMLInputElement} inputRef - 輸入框的元素實體
+   * @memberof TodoListComponent
+   */
+  addTodo(inputRef: HTMLInputElement): void {
+    console.log(inputRef.value);
+    inputRef.value = '';
+  }
+}
