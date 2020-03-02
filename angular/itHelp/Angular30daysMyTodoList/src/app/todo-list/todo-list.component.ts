d872435de1b17452fb44166b9bfa4fa2bf225895
screenleon <screen.leon@gmail.com>
2020-03-02 16:04:18 +0800

    todo-list.service.ts 取得之前儲存的 todo-list todo-list.component.html 假設有大於等於1筆 todo-list, 顯示 todo-list todo-list.component.ts 把值從html 取出並存進 todo-list.service, 把值從 todo-list.service.ts 並傳給html
    新增 javascript/nodejs/practice/.gitignore 忽略 node_modules

--- angular/itHelp/Angular30daysMyTodoList/src/app/todo-list/todo-list.component.ts
+++ angular/itHelp/Angular30daysMyTodoList/src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoListService } from './todo-list.service'
 
 @Component({
   selector: 'app-todo-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private todoListService: TodoListService) { }
 
   ngOnInit(): void {
   }
@@ -18,7 +19,22 @@ export class TodoListComponent implements OnInit {
    * @memberof TodoListComponent
    */
   addTodo(inputRef: HTMLInputElement): void {
-    console.log(inputRef.value);
-    inputRef.value = '';
+
+    const todo = inputRef.value.trim();
+
+    if (todo) {
+      this.todoListService.add(todo);
+      inputRef.value = '';
+    }
+  }
+
+  /**
+   * 取得待辦事項清單
+   *
+   * @returns {string[]}
+   * @memberof TodoListComponent
+   */
+  getList(): string[] {
+    return this.todoListService.getList();
   }
 }
--- angular/itHelp/Angular30daysMyTodoList/src/app/todo-list/todo-list.service.ts
+++ angular/itHelp/Angular30daysMyTodoList/src/app/todo-list/todo-list.service.ts
@@ -13,11 +13,19 @@ export class TodoListService {
  * @memberof TodoListService
  */
   add(title: string): void {
-
     // 避免傳入的 title 是無效值或空白字串，稍微判斷一下
     if (title || title.trim()) {
       this.list.push(title);
     }
+  }
 
+  /**
+   * get todo list
+   * 
+   * @returns {string[]}
+   * @memberof TodoListService
+   */
+  getList(): string[] {
+    return this.list;
   }
 }
