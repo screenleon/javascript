1c54163c82793a3c52854b7e9364a4174b7d2d05
screenleon <screen.leon@gmail.com>
2020-03-02 15:05:33 +0800

    讓 todo-list.component input 在按下enter後能把值傳到 todo-list.service.ts add函式中，保存到陣列中並顯示

---
+++ angular/itHelp/Angular30daysMyTodoList/src/app/todo-list/todo-list.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoListService {
+  private list: string[] = [];
+  constructor() { }
+
+  /**
+ * 新增待辦事項
+ * @param {string} title - 待辦事項的標題
+ * @memberof TodoListService
+ */
+  add(title: string): void {
+
+    // 避免傳入的 title 是無效值或空白字串，稍微判斷一下
+    if (title || title.trim()) {
+      this.list.push(title);
+    }
+
+  }
+}
