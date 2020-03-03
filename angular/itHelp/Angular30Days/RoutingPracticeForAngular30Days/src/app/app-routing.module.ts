3ba0173bb57c80131b42bb6be554acc5cddb3b57
screenleon <screen.leon@gmail.com>
2020-03-03 14:25:22 +0800

    practice router in angular add about.component.ts, home.component.ts to app-routing.module.ts, set component to routes app.component.html write different styles of writing to add routes * if want to use route need to add routing.modules.ts tag in html

---
+++ angular/itHelp/Angular30Days/RoutingPracticeForAngular30Days/src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component'
+import { HomeComponent } from './home/home.component'
+
+// /home, /about
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    // enableTracing: true,
+    // useHash: true
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
