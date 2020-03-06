d060192d6e852418de1b152aa1a2fd49cac67f40
screenleon <screen.leon@gmail.com>
2020-03-06 11:10:42 +0800

    新增別人的模板資訊，並且套用

--- angular/itHelp/Angular30Days/Angular30DaysECApplication/src/app/checkout/customer-info/customer-info.component.ts
+++ angular/itHelp/Angular30Days/Angular30DaysECApplication/src/app/checkout/customer-info/customer-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { appPath } from '../../app-path.const';
 
 @Component({
   selector: 'app-customer-info',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-info.component.css']
 })
 export class CustomerInfoComponent implements OnInit {
+
+  public path = appPath;
 
   constructor() { }
 
--- angular/itHelp/Angular30Days/Angular30DaysECApplication/src/app/checkout/payment-info/payment-info.component.ts
+++ angular/itHelp/Angular30Days/Angular30DaysECApplication/src/app/checkout/payment-info/payment-info.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { appPath } from '../../app-path.const';
+
 @Component({
   selector: 'app-payment-info',
   templateUrl: './payment-info.component.html',
   styleUrls: ['./payment-info.component.css']
 })
 export class PaymentInfoComponent implements OnInit {
-
+  public path = appPath;
   constructor() { }
 
   ngOnInit() {
