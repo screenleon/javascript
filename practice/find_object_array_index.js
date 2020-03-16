b9e17c4df4aee102c26ffc8e57f7575477c5e46e
screenleon <screen.leon@gmail.com>
2020-03-16 11:39:56 +0800

    練習從物件陣列中取出特定值的物件索引

---
+++ practice/find_object_array_index.js
@@ -0,0 +1,12 @@
+const objectArray = [
+    {
+        name: 'lien',
+        gender: 1
+    },
+    {
+        name: 'chen',
+        gender: 1
+    }
+];
+
+const tempIndex = this.objectArray.findIndex(object => object.name === 'chen');
