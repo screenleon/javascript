5fa6fd0e5710c95b295ad307344e04ba9d5543c3
screenleon <screen.leon@gmail.com>
2020-12-31 11:33:09 +0800

    Add filter and sort feature

--- library/fetchYtList.js
+++ library/fetchYtList.js
@@ -2,17 +2,33 @@ const ytsr = require('ytsr');
 
 const argv = process.argv.slice(2);
 
+const filterString = argv[1];
+const sort = argv[2];
+
 ytsr(argv[0])
    .then(response => {
-      const result = [];
+      let result = [];
       response.items.forEach(element => {
-         if (!element.duration || element.isUpcoming) return;
+         // if (!element.duration || element.isUpcoming) return;
          result.push({
-            title: element.title,
+            title: String(element.title).replace(/(^|\D)(\d)(?!\d)/g, "$10$2"),
             date: element.uploadedAt,
             url: element.url,
             duration: element.duration
          })
       })
-      console.log(result);
+
+      if(!!filterString) {
+         result = result.filter(element => {
+            return String(element.title).search(filterString) > -1;
+         });
+      }
+      
+      if(sort === "true" || sort === "1") {
+         result.sort(function (a, b) {
+            return (a.title > b.title) ? 1 : -1;
+         })
+      }
+      
+      console.log(JSON.stringify(result));
    })
